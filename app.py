126c2395a6f67d9f397e3aa42f65c50b4a5022a0
hyc449 <hyc449@nyu.edu>
2019-10-15 19:31:59 -0600

    Updated App.py

---
+++ app.py
@@ -0,0 +1,122 @@
+import os
+from flask import Flask, abort, request, jsonify, g, url_for, redirect, escape, render_template, flash
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, IntegerField, widgets
+#from itsdangerous import (TimedJSONWebSignatureSerializer
+#                          as Serializer, BadSignature, SignatureExpired)
+                          
+app = Flask(__name__)
+SECRET_KEY = b'?\x03?w*\xd2\x84\xea\xc3\xc1\x8c\xe7\x80\x83\x9d\x8c=\xb1\x17\xe3Z\xf4|C'
+credential_dictionary = {}
+
+class RegistrationForm(Form):
+    uname = StringField('Username', [validators.Length(min=4, max=25)], id='uname')
+    pword = PasswordField('New Password', [validators.DataRequired()], id='pword')
+    phone = StringField('Phone Number', [validators.Length(min=10, max=10), validators.DataRequired()], id='2fa')
+    #phone = IntegerField('Phone', [validators.NumberRange(min=0, max=10), validators.DataRequired()], id='2fa', widget = widgets.Input(input_type="tel"))
+    #confirm = PasswordField('Repeat Password')
+    #accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+
+def reformat_phone(form, field):
+    field.data = field.data.replace('-', '')
+    return True
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login(message=None):
+    if request.method == 'POST':
+        #session['username'] = request.form['username']
+        #return redirect(url_for('index'))
+        username = request.form.get('uname')
+        password = request.form.get('pword')
+        phone = request.form.get('2fa')
+        print (username, password, phone)
+        if username in credential_dictionary:
+            if password == credential_dictionary[username][0] and phone == credential_dictionary[username][1]:
+                print ("Login successful")
+                return '''
+                    <p>Login Successful</p>
+                '''
+            else:
+                print ("Login failed")
+        else:
+            print ("Login failed")
+    if message==True:
+        return '''
+        <h1 id='success'>
+        <form action = "" method = "post">
+            <p>UserName<input id = uname type = text name = uname></p>
+            <p>Password<input id = pword type = password name = pword></p>
+            <p>Phone<input id = 2fa type = text name = 2fa></p>
+            <p><input type = submit value = Login/></p>
+        </form>
+        
+        '''
+    else:
+        return '''
+        
+        <form action = "" method = "post">
+            <p>UserName<input id = uname type = text name = uname></p>
+            <p>Password<input id = pword type = password name = pword></p>
+            <p>Phone<input id = 2fa type = text name = 2fa></p>
+            <p><input type = submit value = Login/></p>
+        </form>
+        
+        '''
+
+#@app.route('/register', methods = ['GET','POST'])
+#def register():
+#    if request.method == 'POST':
+#        username = request.form.get("uname")
+#        password = request.form.get("pword")
+#        phone = request.form.get("phone")
+#        #print("username: ", username, "\n")
+#        #print("password: ", password, "\n")
+#        #with open("test.txt","w+") as fo:
+#        #    fo.write("username: %s\r\n" % username)
+#        #    fo.write("password: %s\r\n" % password)    
+#    return '''
+#   <form method = "POST">
+#      <p>UserName<input id = uname type = text name = uname></p>
+#      <p>Password<input id = pword type = password name = pword></p>
+#      <p>Password<input id = pword type = password name = pword></p>
+#      <p><input type = submit value = Login></p>
+#   </form>
+#    '''
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        #user = User(form.uname.data, form.pword.data,
+                    #form.phone.data)
+        #db_session.add(user)
+        credential_dictionary[form.uname.data] = [form.pword.data, form.phone.data]
+        flash('Thanks for registering')
+        print (credential_dictionary[form.uname.data][0], credential_dictionary[form.uname.data][1])
+        registered = True
+        return redirect(url_for('login', message=registered))
+        #return redirect(url_for('login'))
+    return render_template('register.html', form=form) 
+ 
+@app.route('/success')
+def success():
+    return '''
+    <p id="success">Registered successfully</p>
+    '''
+ 
+@app.route('/api/users', methods = ['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400) # missing arguments
+    if User.query.filter_by(username = username).first() is not None:
+        abort(400) # existing user
+    user = User(username = username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({ 'username': user.username }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
+
+if __name__ == '__main__':
+    app.secret_key = SECRET_KEY
+    app.run(debug=True, host='127.0.0.1', port="5001")
