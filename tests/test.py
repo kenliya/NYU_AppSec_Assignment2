c432ad89a6b2fd669eb0000cd6390ed0f32773c8
hyc449 <hyc449@nyu.edu>
2019-10-27 17:19:20 -0600

    Updated test cases

--- tests/test.py
+++ tests/test.py
@@ -7,22 +7,34 @@ sys.path.insert(0, myPath + '/../')
 
 class FlaskTestCase(unittest.TestCase):
     def setUp(self):
-        #app.config['TESTING'] = True
-        self.app = app.app.test_client()
+        self.app = app.app.test_client() 
+        #self.app.config['TESTING'] = True
+        self.app.application.config['WTF_CSRF_METHODS'] = []
         self.app.testing = True
         self.app.debug = True
-        
-	#def test_register(self):
-	#	res = self.web.get("/register")
-	#	assert res.status_code == 200
     
     def tearDown(self):
         pass
     
+    def test_register_page(self):
+        res = self.app.get("/register")
+        print(res)
+        assert res.status_code == 200
+    
     def test_register(self):
         test_data = {'uname': "test", 'pword':"1234567", 'phone': "1234567890"}
         response = self.app.post('/register', data=test_data)
-        self.assertIn('failure', response.data)
+        print(response)
+        self.assertIn(b'hidden', response.data)
+    
+    def test_login(self):
+        test_data = {'uname': "test", 'pword':"1234567", 'phone': "1234567890"}
+        response = self.app.post('/register', data=test_data)
+        print(response)
+        response = self.app.post('/login', data = test_data)
+        print(response)
+        self.assertIn(b'result', response.data)
+        
     
     #def test_statuscode(self):
         
